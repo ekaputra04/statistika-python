eca0c0799e7010e1915de4f00ed8446ffbba0b49
ekaputra04 <ekaputrajuniawan@gmail.com>
2024-03-23 17:08:31 +0800

    feat: z score calculator, fix z score probability

--- z-score-calculator/z-score-calculator.py
+++ z-score-calculator/z-score-calculator.py
@@ -5,21 +5,19 @@ import matplotlib.pyplot as plt
 # online:
 # https://www.calculator.net/z-score-calculator.html
 
-def draw_z_score(x, cond, mu=0, sigma=1):
-    y = stats.norm.pdf(x, mu, sigma)
-    z = x[cond]
-    plt.plot(x, y)
-    plt.fill_between(z, 0, stats.norm.pdf(z, mu, sigma))
-    plt.show()
+def hitung_z_score(x, μ, σ):
+    z_score = (x - μ) / σ    
+    return z_score
     
 def main():
-    z_score = input("Masukkan Z-score : ")
-    z_score = float(z_score)
-    p = stats.norm.cdf(z_score)
-    print("Probability : ", p)
-    
-    x = np.arange(-3, 3, 0.001)
-    draw_z_score(x, x<z_score)
+    x = input("Masukkan nilai X : ")
+    x = float(x)
+    μ = input("Masukkan nilai μ : ")
+    μ = float(μ)
+    σ = input("Masukkan nilai σ : ")
+    σ = float(σ)
+    z_score = hitung_z_score(x, μ, σ)
+    print("Nilai Z Score :", z_score)
 
 if __name__ == "__main__":
     main()
